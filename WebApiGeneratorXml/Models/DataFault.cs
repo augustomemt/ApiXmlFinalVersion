c8add1a75f6513dbf15d90c9c3dd45bfe2995a79
augustomemt <augusto.morais@memt.com.br>
2020-10-19 12:36:03 -0300

    First commit

---
+++ WebApiGeneratorXml/Models/DataFault.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiGeneratorXml.Models
+{
+    public class DataFault
+    {
+        public int Dia { get; set; }
+        public int Hora { get; set; }
+        public int NReg { get; set; }
+
+        public DateTime DataHora { get; set; }
+    }
+}
